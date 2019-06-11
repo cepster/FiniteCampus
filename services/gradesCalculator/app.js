8447736734306aca4e685b3fcdc8b712575aa553
Matt Richards <matt.richards@infinitecampus.com>
2019-06-11 13:52:52 -0500

    Calculate grades

--- services/gradesCalculator/app.js
+++ services/gradesCalculator/app.js
@@ -21,10 +21,37 @@ app.listen(port, () =>
   console.log(`Grades Calculator Service listening on port ${port}!`)
 );
 
-let calculateGrades = () => {
+let calculateGrades = newScores => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      newScores.forEach(score => {
+        grades = grades.map(g => {
+          if (g.studentName === score.studentName) {
+            g.grade = getRandomGrade();
+          }
+
+          return g;
+        });
+      });
       resolve();
     }, 5000);
   });
+};
+
+let getRandomGrade = () => {
+  return [
+    "A+",
+    "A",
+    "A-",
+    "B+",
+    "B",
+    "B-",
+    "C+",
+    "C",
+    "C-",
+    "D+",
+    "D",
+    "D-",
+    "F"
+  ][Math.floor(Math.random() * 13)];
 };
