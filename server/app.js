2edd710f6b804e8c97cd952bfb5713783f45c7f1
Matt Richards <matt.richards@infinitecampus.com>
2019-06-11 23:06:23 -0500

    Checkpoint

--- server/app.js
+++ server/app.js
@@ -2,47 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 
-let newNotifications = [];
-let grades = [
-  {
-    id: 1,
-    studentName: "Bart Simpson",
-    grade: "F"
-  },
-  {
-    id: 2,
-    studentName: "Milhouse Van Houten",
-    grade: "C"
-  },
-  {
-    id: 3,
-    studentName: "Lisa Simpson",
-    grade: "A+"
-  },
-  {
-    id: 4,
-    studentName: "Martin Prince",
-    grade: "A"
-  }
-];
-
 const app = express();
 app.use(cors());
 
-app.get("/helloWorld", (req, res) => {
-  res.status(200).send("Hello!");
-});
-
+/*
+ * GET /grades
+ */
 app.get("/grades", (req, res) => {
   res.status(200).send(grades);
 });
 
+/*
+ * GET /newNotifications
+ */
 app.get("/newNotifications", (req, res) => {
   res.status(200).send(newNotifications);
   newNotifications = [];
 });
 
+/*
+ * POST /saveGradebook
+ * Updates gradebook scores, calls out to grades calculator, and triggers notifications
+ */
 app.post("/saveGradebook", bodyParser.json(), (req, res) => {
+  saveGradebook();
   calculateGrades(req.body).then(() => {
     sendNotifications(req.body);
     res.status(200).send(req.body);
@@ -66,6 +49,10 @@ let calculateGrades = newScores => {
       resolve();
     }, 5000);
   });
+};
+
+let saveGradebook = () => {
+  console.log("Saving Grade Book!");
 };
 
 let sendNotifications = newScores => {
@@ -96,3 +83,31 @@ let getRandomGrade = () => {
     "F"
   ][Math.floor(Math.random() * 13)];
 };
+
+/*
+ * Data
+ */
+
+let newNotifications = [];
+let grades = [
+  {
+    id: 1,
+    studentName: "Bart Simpson",
+    grade: "F"
+  },
+  {
+    id: 2,
+    studentName: "Milhouse Van Houten",
+    grade: "C"
+  },
+  {
+    id: 3,
+    studentName: "Lisa Simpson",
+    grade: "A+"
+  },
+  {
+    id: 4,
+    studentName: "Martin Prince",
+    grade: "A"
+  }
+];
